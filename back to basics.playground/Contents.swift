25deefdc76442ac137e6819cbb811639357cca1e
West Kraemer <kraemerwest@gmail.com>
2018-11-27 11:44:03 -0500

    This is a file dedicated to practicing the basics of the Swift language to stay sharp and to improve my fundamentals.

---
+++ back to basics.playground/Contents.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+var str = "Hello, playground"
+
+var friendsList = ["Monica", "Chandler", "Ross", "Rachel", "Phoebe", "Joey"]
+
+for name in friendsList {
+    if name == "Monica" {
+        print(name + " was fat")
+    } else {
+        print("not Monica")
+    }
+}
